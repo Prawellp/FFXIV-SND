5114c1e7783600221afaf74e9037607a28f0a86a
pot0to <xxuemma@gmail.com>
2024-08-24 13:56:23 -0700

    updating mintimelefttoignore

--- Fate Farming.lua
+++ Fate Farming.lua
@@ -101,7 +101,7 @@ OldV = false               --should it Exchange Old Vouchers
 --Fate settings
 WaitIfBonusBuff = true          --Don't change instances if you have the Twist of Fate bonus buff
 CompletionToIgnoreFate = 80     --Percent above which to ignore fate
-MinTimeLeftToIgnoreFate = 14.5*60  --Seconds below which to ignore fate
+MinTimeLeftToIgnoreFate = 4*60  --Seconds below which to ignore fate
 JoinBossFatesIfActive = true    --Join boss fates if someone is already working on it (to avoid soloing long boss fates). If false, avoid boss fates entirely.
 CompletionToJoinBossFate = 20   --Percent above which to join boss fate
 fatewait = 0                    --the amount how long it should when before dismounting (0 = at the beginning of the fate 3-5 = should be in the middle of the fate)
